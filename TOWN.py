91cef7be5c6200aa82816f26cba3abf79ce6a50c
pickyt <picky.games.ch@gmail.com>
2021-05-14 00:00:19 +1000

    added marriage birth and relationship and inventory system also seperated first name last name and other changes

--- TOWN.py
+++ TOWN.py
@@ -2,13 +2,69 @@ import random
 import math
 
 meeple = []
+resource = []
 class Meeple:
-    def __init__(self,name,sex,age,job,bal):
-        self.name = name
+    def __init__(self,firstname,lastname,sex,age,job,bal):
+        self.fname = firstname
+        self.lname = lastname
+        self.name = firstname+" "+lastname
         self.sex = sex
         self.age = age
         self.job = job
-        self.bal = bal
+        self.married = False
+        self.partner = []
+        self.children = []
+        self.inventory = {
+         "bal" : bal
+        }
+    def addItem(self,item,amount):
+        if item not in self.inventory:
+            self.inventory.update({item:amount})
+        else:
+            self.inventory.update({item:self.inventory[item]+amount})
+    def subtractItem(self,item,amount):
+        self.inventory.update({item:self.inventory[item]-amount})
+
+    def marry(self,other):
+        if not self.married:
+            if self.sex == "Male":
+                other.lname = self.lname
+                other.partner = self.name
+                updatenames(meeple)
+                self.partner = other.name
+                self.married = True
+
+            else:
+                self.lname = other.lname
+                other.partner = self.name
+                updatenames(meeple)
+                self.partner = other.name
+                self.married = True
+
+
+    def haveChild(self,other,children):
+        if self.married :
+            for item in range(children):
+                if random.randrange(2) == 1:
+                    sex = "Male"
+                else:
+                    sex = "Female"
+                self.children = Meeple(getName(sex)[0],self.lname,sex,0,"unemployed",0)
+                meeple.append(self.children)
+        else:
+            print('Married'+ self.married)
+
+
+
+class Resource:
+    def __init__(self,name,rarity):
+        self.name = name
+        self.rarity =rarity
+
+
+def updatenames(obj):
+    for i in obj:
+        i.name = i.fname + " " + i.lname
 
 def generateMeeples(num):
     ageSD = 8
@@ -28,7 +84,8 @@ def generateMeeples(num):
         if newname not in names:
             newname = getName(sex)
         names.append(newname)
-        meeple.append(Meeple(newname,sex,age,job,bal))
+        meeple.append(Meeple(newname[0],newname[1],sex,age,job,bal))
+
 def getName(sex):
     l = open("assets/names/lastnames.txt","r")
     last = l.read().split("\n")
@@ -38,12 +95,12 @@ def getName(sex):
     else:
         f= open("assets/names/femalefirstnames.txt","r")
         first = f.read().split("\n")
-    return first[random.randrange(len(first)-1)]+" "+last[random.randrange(len(last)-1)]
+    return [first[random.randrange(len(first)-1)],last[random.randrange(len(last)-1)]]
 
 def listMeeples(option = "all"):
     for i in meeple:
         if  option == "all":
-            print(f'name:{i.name} Sex:{i.sex} Age:{i.age} Occupation:{i.job} Balance:{i.bal}')
+            print(f' name: {i.name} \n Sex: {i.sex}     Age: {i.age}    Occupation: {i.job}     Balance: ${i.inventory["bal"]}   Married to: {i.partner} \n Inventory : {i.inventory}\n')
         elif option == "names":
             print(i.name)
         elif option == "sex":
@@ -53,8 +110,18 @@ def listMeeples(option = "all"):
         elif option == "job":
             print(f'{i.name} is a {i.job}')
         elif option == "bal":
-            print(f'{i.name} has {i.bal}')
-        # elif option == "rel":
-        #     print(listrelatives())
-generateMeeples(100)
+            print(f'{i.name} has ${i.inventory["bal"]}')
+        elif option == "inv":
+            print(f'{i.name} inventory: {i.inventory}')
+        elif option == "fname":
+            print(f' first name is {i.fname}')
+        elif option == "lname":
+            print(f' {i.fname}s last name is {i.lname}')
+
+generateMeeples(4)
 listMeeples("all")
+print(len(meeple))
+meeple[len(meeple)-1].marry(meeple[len(meeple)-2])
+meeple[len(meeple)-1].haveChild(meeple[len(meeple)-2],2)
+listMeeples("all")
+print(len(meeple))
